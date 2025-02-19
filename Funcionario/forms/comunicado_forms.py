1d353176692ce715f5e86ec41597eb00ecc96191
Rafael Almeida <dealmeida.silva@hotmail.com>
2025-02-19 07:21:06 -0300

    finalizado

--- Funcionario/forms/comunicado_forms.py
+++ Funcionario/forms/comunicado_forms.py
@@ -3,10 +3,8 @@ from ..models import Comunicado
 from django_ckeditor_5.widgets import CKEditor5Widget
 
 class ComunicadoForm(forms.ModelForm):
-    descricao = forms.CharField(
-        widget=CKEditor5Widget(config_name='default'),  # Certifique-se de usar o mesmo config_name
-        required=True
-    )
+    descricao = forms.CharField(widget=CKEditor5Widget(config_name='default'))
+
  
     class Meta:
         model = Comunicado
