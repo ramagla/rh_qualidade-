b7f859d8ccf8ede83e348e39b7bd4b47150721c4
Rafael Almeida <dealmeida.silva@hotmail.com>
2025-06-23 16:07:12 -0300

    finalizado a importação dos recibos de pagamento

--- Funcionario/templatetags/conversores.py
+++ Funcionario/templatetags/conversores.py
@@ -1,13 +1,16 @@
 from django import template
+from babel.numbers import format_currency
 
 register = template.Library()
 
-
 @register.filter
 def horas_formatadas(duracao_decimal):
-    horas = int(duracao_decimal)
-    minutos = int((duracao_decimal - horas) * 60)
-    return f"{horas:02}:{minutos:02}"
+    try:
+        horas = int(duracao_decimal)
+        minutos = int((duracao_decimal - horas) * 60)
+        return f"{horas:02}:{minutos:02}"
+    except:
+        return "00:00"
 
 @register.filter
 def traduzir_mes(valor):
@@ -16,7 +19,17 @@ def traduzir_mes(valor):
         "Jun": "Jun", "Jul": "Jul", "Aug": "Ago", "Sep": "Set",
         "Oct": "Out", "Nov": "Nov", "Dec": "Dez"
     }
-    partes = valor.split("/")
-    mes = mapa.get(partes[0], partes[0])
-    return f"{mes}/{partes[1]}"
+    try:
+        partes = valor.split("/")
+        mes = mapa.get(partes[0], partes[0])
+        return f"{mes}/{partes[1]}"
+    except:
+        return valor
 
+
+@register.filter
+def br_currency(value):
+    try:
+        return format_currency(value or 0, 'BRL', locale='pt_BR')
+    except:
+        return "R$ 0,00"
