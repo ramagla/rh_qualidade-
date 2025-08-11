8c9d23d75fba113bfb04e94ed6e03d2cb6437956
Rafael Almeida <dealmeida.silva@hotmail.com>
2025-08-11 11:19:53 -0300

    adicionando alertas

--- alerts/admin.py
+++ alerts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Alerta
+from .models import Alerta, AlertaConfigurado, AlertaUsuario
 
 
 @admin.register(Alerta)
@@ -8,3 +8,18 @@ class AlertaAdmin(admin.ModelAdmin):
     list_display = ("nome", "ativo", "descricao")
     list_filter = ("ativo",)
     search_fields = ("nome", "descricao")
+
+@admin.register(AlertaConfigurado)
+class AlertaConfiguradoAdmin(admin.ModelAdmin):
+    list_display    = ("tipo", "ativo")
+    list_filter     = ("ativo",)
+    search_fields   = ("tipo",)
+    filter_horizontal = ("usuarios", "grupos")
+
+@admin.register(AlertaUsuario)
+class AlertaUsuarioAdmin(admin.ModelAdmin):
+    list_display   = ("titulo", "usuario", "tipo", "criado_em", "lido")
+    list_filter    = ("tipo", "lido", "excluido")
+    search_fields  = ("titulo", "mensagem", "usuario__username")
+    readonly_fields = ("criado_em",)
+    date_hierarchy = "criado_em"
